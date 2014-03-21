fe2457ac77f0dc96b18dd671c822725cb5ef8f79
sreejithsreekantan <krssreejith@gmail.com>
2014-03-21 22:05:50 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,28 @@
 from setuptools import setup
  
 setup(
-    name = 'JSONXMLConverter',
+    name = 'jsonxmlconverter',
     packages = ['jsonxmlconverter'],
     version = '0.0.1',
     description = 'Converts JSON file into XML',
     author='Sreejith Sreekantan',
     author_email='krssreejith@gmail.com',
     url='https://github.com/sreejithsreekantan/jsonxmlconverter',
+    license='LICENSE.txt',
+    long_description=open('README.txt').read(),
     classifiers=[
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
         'License :: OSI Approved :: GNU General Public License (GPL)',
         'Operating System :: OS Independent',
-        'Development Status :: 1 - Planning',
+        'Development Status :: 4 - Beta',
         'Environment :: Console',
         'Intended Audience :: Developers',
-        'Topic :: Programming :: GIS'
+        'Topic :: Scientific/Engineering :: Visualization'
+    ],
+    install_requires=[
+        "sys",
+        "getopt",
+        "json"
     ]
 )
